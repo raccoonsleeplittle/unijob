eaa3ad117c5c01e4548cdf5633f55e76eed28e24
KUNNOBI <nonkungamer12@gmail.com>
2022-08-10 20:06:39 +0700

    about2

--- about.php
+++ about.php
@@ -18,6 +18,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+        
     </head>
 
 <body id="page-top" style="background-image: url('./img/bg-img.jpg');">
@@ -48,13 +49,191 @@
                 <!-- Masthead-->
                 <header class="masthead">
             <div class="container" style="text-align: center;">
+            <img src="./img/Logo.png" width="250px" height="auto" >
                 <div class="masthead-heading text-uppercase" style="text-align: center;">About</div>
-                <div  class="masthead-subheading " style="text-align: center;">เนื่องจากเราได้มองเห็นปัญหา ณ ปัจจุบัน ที่มีทั้งค่าครองชีพที่สูงและอีกทั้งยังมีเศรษฐกิจที่ย่ำแย่ จนส่งผลกระทบทางด้านการเงินต่อครอบครัวของนักศึกษาจึงทำให้เราเกิด
-คอนเซ็ปต์ไอเดียที่อยากให้นักศึกษามีรายได้และสามารถสำเร็จการศึกษา
+                <div  class="masthead-subheading " style="text-align: center;font-size:25px; ">เนื่องจากเราได้มองเห็นปัญหา ณ ปัจจุบัน<br>ที่มีทั้งค่าครองชีพที่สูงและอีกทั้งยังมีเศรษฐกิจที่ย่ำแย่<br>จนส่งผลกระทบทางด้านการเงินต่อครอบครัวของนักศึกษาจึงทำให้เราเกิด<br>
+คอนเซ็ปต์ไอเดียที่อยากให้นักศึกษามีรายได้และสามารถสำเร็จการศึกษา<br>
 ได้โดยไม่มีอุปสรรคทางด้านการเงิน</div>
             </div>
+            <section class="page-section" id="promotion">
+            <div class="container">
+                <div class="text-center">
+                    <h1 style="font-family: 'Kanit', sans-serif;color:white;">โปรโมชั่นรายวัน</h1> 
+                </div>
+                <div class="row">
+                    <div class="col-10 mx-auto">
+                        <div id="promotion" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#promotion" data-slide-to="0" class="active"></li>
+                                <li data-target="#promotion" data-slide-to="1"></li>
+                                <li data-target="#promotion" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="./img/new1.jpg" alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="./img/new2.jpg" alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="./img/new3.jpg" alt="Third slide">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#promotion" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#promotion" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="page-section" id="promotion">
+            <div class="container">
+                <div class="text-center">
+                    <h1 style="font-family: 'Kanit', sans-serif;color:white;">โปรโมชั่นรายวัน</h1> 
+                </div>
+                <div class="row">
+                    <div class="col-10 mx-auto">
+                        <div id="promotion" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#promotion" data-slide-to="0" class="active"></li>
+                                <li data-target="#promotion" data-slide-to="1"></li>
+                                <li data-target="#promotion" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="./img/new1.jpg" alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="./img/new2.jpg" alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="./img/new3.jpg" alt="Third slide">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#promotion" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#promotion" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         </header>
 
+        <!--footer-->
+        <!-- Remove the container if you want to extend the Footer to full width. -->
+        <footer class="bg-dark text-center text-lg-start text-white">
+            <!-- Grid container -->
+            <div class="container p-4">
+                <!--Grid row-->
+                <div class="row mt-4">
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h1 class="text-uppercase" style="color:#4785ff">UNIJOB</h1>
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a>บริษัท  เซนติกา โกลบอล เทค จำกัด 
+                                    39 หมู่ที่ 1 ถนน รังสิต - นครนายก 
+                                    ตำบล คลองหก อำเภอคลองหลวง 
+                                    ปทุมธานี 12110
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0" style="text-align:center;">
+                        <h4 class="text-uppercase" style="font-family:'Kanit',sans-serif;">เมนู</h4>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a>หน้าแรก</a>
+                            </li>
+                            <li>
+                               <a href="#" style="color:white;font-family:'Kanit',sans-serif;">เกี่ยวกับ</a>
+                            </li>
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">ข่าวสาร</a>
+                            </li>
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">รีวิว</a>
+                            </li>
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">ดาวน์โหลด</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0" style="text-align:center;">
+                        <h5 class="text-uppercase" style="font-family:'Kanit',sans-serif;" >ศูนย์ช่วยเหลือ</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">ติดต่อรา</a>
+                            </li>
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">วิธีใช้งาน</a>
+                            </li>
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">คำถามที่พบบ่อย</a>
+                            </li>
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">เงื่อนไขและข้อกำหนด</a>
+                            </li>
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">นโยบายความเป็นส่วนตัว</a>
+                            </li>
+                            <li>
+                                <a href="#" style="color:white;font-family:'Kanit',sans-serif;">นโยบายการใช้ข้อมูลคุกกี้</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0" style="text-align:center;">
+                        <h5 class="text-uppercase" style="font-family:'Kanit',sans-serif;">ติดต่อเรา</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a>ติดต่อสอบถามข้อมูลเพิ่มเติม</a>
+                            </li>
+                            <li>
+                                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                                <!-- Twitter -->
+                                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                                <!-- Google -->
+                                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                                    <i class="fab fa-google"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a>Info@unijob.com</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--Grid row-->
+            </div>
+            <!-- Grid container -->
+            <!-- Copyright -->
+            <div class="text-center p-1" style="background-color: rgba(0, 0, 0, 0.2)">
+                Copyright © www.unijob.com <br>เพราะเราอยากให้นักศึกษามีรายได้และสามารถสำเร็จการศึกษาได้โดยไม่มีอุปสรรคทางด้านการเงิน คึดถึงงานคิดถึง Unijob
+                <a class="text-secondary" href="https://www.unijob.com/">Unijob.com</a>
+            </div>
+        <!-- Copyright -->
+        </footer>
 
     
 </body>
